4a014936569ae178ae5dc29a5f715a3e6568de05
yashdha1 <yashdhadod04@gmail.com>
2024-02-14 13:56:46 +0530

    few fixex

--- login/logic.js
+++ login/logic.js
@@ -2,20 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var form = document.querySelector('.login-container form');
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent default form submission
-        // Get username and password values from the input fields
+        event.preventDefault();  
         var usernameInput = document.querySelector('input[type="text"]');
         var passwordInput = document.querySelector('input[type="password"]');
 
         var username = usernameInput.value;
         var password = passwordInput.value;
-
-        // Check if username and password are correct
+        
         if (username === "yashdhadod" && password === "123") {
             window.history.back();
         } else {
             alert("Invalid username or password. Please try again.");
-            // Clear the input fields
             usernameInput.value = '';
             passwordInput.value = '';
         }
